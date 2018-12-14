d7246fcada929802e84b33eddac0801f59a3c5b0
Your Name <you@example.com>
2018-12-14 18:18:48 +0400

    add

--- Lesson4/script.js
+++ Lesson4/script.js
@@ -1,38 +1,40 @@
 var v = 0;
 function main() {
-   var socket = io();
-   var chatDiv = document.getElementById('chat');
-   var input = document.getElementById('message');
-   var button = document.getElementById('submit');
-   var remove = document.getElementById('remove');
+    var socket = io();
+    var chatDiv = document.getElementById('chat');
+    var input = document.getElementById('message');
+    var button = document.getElementById('submit');
+    var remove = document.getElementById('remove');
 
-   function handleSubmit(evt) {
-       var val = input.value;
-       if (val != "") {
-           socket.emit("send message", val);
-       }
-   }
-    function deleteSubmit(evt) {
-       var val = input.value;
-        socket.emit("remove message", true);
-   }
-   button.onclick = handleSubmit;
-   remove.onclick = deleteSubmit;
-   function handleMessage(msg) {
-        var np = document.getElementsByTagName("p");
-        if(typeof msg == "boolean" && msg){
-            chatDiv.removeChild(np[np.length - 1]);
+    function handleSubmit(evt) {
+        var val = input.value;
+        if (val != "") {
+            socket.emit("send message", val);
         }
-        else{
-        console.log(msg);
+    }
+    function handleDelete() {
+        socket.emit("remove message");
+    }
+    button.onclick = handleSubmit;
+    remove.onclick = handleDelete;
+
+    function handleMessage(msg) {
         var p = document.createElement('p');
         p.innerText = msg;
         chatDiv.appendChild(p);
         input.value = "";
+    }
+    function deleteFromDom() {
+        var p = document.getElementsByTagName("p");
+        for (var i in p) {
+            if (p.length > 0) {
+                chatDiv.removeChild(p[0]);
+            }
         }
     }
 
-socket.on('display message', handleMessage);
+    socket.on('display message', handleMessage);
+    socket.on("de dzer motic jnjeq", deleteFromDom);
 } // main closing bracket
 
 window.onload = main;
--- Lesson4/server.js
+++ Lesson4/server.js
@@ -6,20 +6,20 @@ var messages = [];
 
 app.use(express.static("."));
 app.get('/', function (req, res) {
-   res.redirect('index.html');
+    res.redirect('index.html');
 });
 server.listen(3000);
 
 io.on('connection', function (socket) {
-   for(var i = 0; i < messages.length; i++) {
-     io.sockets.emit("display message", messages[i]);
-   }
-   socket.on("send message", function (data) {
-       messages.push(data);
-       io.sockets.emit("display message", data);
-   });
-   socket.on("remove message", function (data) {
-       messages.splice(messages.length-1,1);
-       io.sockets.emit("display message", data);
-   });
+    for (var i = 0; i < messages.length; i++) {
+        io.sockets.emit("display message", messages[i]);
+    }
+    socket.on("send message", function (data) {
+        messages.push(data);
+        io.sockets.emit("display message", data);
+    });
+    socket.on("remove message", function () {
+        messages = [];
+        io.sockets.emit("de dzer motic jnjeq");
+    });
 });
